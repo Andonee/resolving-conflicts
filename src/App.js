2a33c9330bb96186e962a1847f1a15b706403f8c
mateusz.poprawa <mateusz.poprawa@autenti.com>
2023-04-09 09:38:34 +0200

    some  changes on dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsdfsdf
+          Some changes on dev
         </a>
       </header>
     </div>
